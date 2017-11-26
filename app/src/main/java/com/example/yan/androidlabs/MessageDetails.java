36639d3ef8414751022461dcd3cd3580fda1bcfc
Yan <yan.cheng@hotmail.ca>
2017-11-26 14:43:35 -0500

    Finished lab 7

--- app/src/main/java/com/example/yan/androidlabs/MessageDetails.java
+++ app/src/main/java/com/example/yan/androidlabs/MessageDetails.java
@@ -9,5 +9,21 @@ public class MessageDetails extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_details);
+
+        //btDelete = (Button) findViewById(R.id.)
+        String message   = getIntent().getStringExtra("message");
+        String messageId = getIntent().getStringExtra("messageId");
+
+        Bundle bundle = new Bundle();
+        //String myMessage = "Stackoverflow is cool!";
+        bundle.putString("message", message );
+
+        bundle.putString("messageId", messageId );
+        MessageFragment messageFragment = MessageFragment.newInstance(null);//if(chatWindow==null) on phone
+        //messageFragment.myText1.setText(string);
+        //messageFragment.myText2.setText(messageId);
+        messageFragment.setArguments(bundle);
+        getFragmentManager().beginTransaction().add(R.id.fragment_container, messageFragment).commit();//in layout/activity_message_details.xml
+
     }
 }
